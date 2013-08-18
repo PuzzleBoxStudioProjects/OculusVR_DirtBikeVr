1da1a48421eb6b8b029b16d4939d501b00936f34
Drew <dre38w@gmail.com>
2013-08-18 00:30:01 -0400

    steering

--- Assets/Drew Stuff/DrewBikePhysics.cs
+++ Assets/Drew Stuff/DrewBikePhysics.cs
@@ -1,42 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DrewBikePhysics : MonoBehaviour {
+public class DrewBikePhysics : MonoBehaviour
+{
 
     public float speed = 10.0f;
+    public float maxSpeed = 20.0f;
+    public float minSpeed = 10.0f;
     public float steerSpeed = 10.0f;
     public float steerAngle = 30.0f;
 
 	public Transform backTire;
 	public Transform frontTire;
 
-    private Vector3 orientation = Vector3.zero;
+    private Vector3 rotDir;
 
-    void Awake() {
-        orientation = transform.eulerAngles;
-    }
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-
-    void Update() {
-        //transform.eulerAngles = orientation;
-    }
-
-	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate ()
+    {
         Movement();
 	}
 
-    void Movement() {
+    void Movement()
+    {
+        //get inputs
         float forwardInput = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         float steer = Input.GetAxis("Horizontal");
 
-        Vector3 rotDir = new Vector3(backTire.eulerAngles.x, backTire.eulerAngles.y, Mathf.LerpAngle(backTire.eulerAngles.z, -steerAngle * steer, 0.3f));
-        backTire.eulerAngles = rotDir;
-
-        backTire.rigidbody.AddForce(backTire.forward * forwardInput, ForceMode.Impulse);
+        //bank the bike
+        rotDir = new Vector3(transform.eulerAngles.x, rotDir.y, Mathf.LerpAngle(transform.eulerAngles.z, -steerAngle * steer, 0.3f));
+        //turn the bike
+        rotDir.y += steer * steerSpeed;
+        
+        //apply the rotation values
+        transform.eulerAngles = rotDir;
+        
+        Vector3 vel = rigidbody.velocity;
+        //set max and min velocities
+        vel.z = Mathf.Clamp(vel.z, -minSpeed, maxSpeed);
+        rigidbody.velocity = vel;
+        
+        //move forward
+        rigidbody.AddForce(transform.forward * forwardInput, ForceMode.Impulse);
     }
 }
