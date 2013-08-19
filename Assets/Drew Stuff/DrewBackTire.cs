2d43457d49cc83424c952065dfdce0bb92e82e10
Drew <dre38w@gmail.com>
2013-08-19 00:51:23 -0400

    good shit

---
+++ Assets/Drew Stuff/DrewBackTire.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrewBackTire : MonoBehaviour
+{
+    //[HideInInspector]
+    public bool isGrounded = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+
+    void LateUpdate()
+    {
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hitInfo, 1))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
--- Assets/Drew Stuff/DrewBikePhysics.cs
+++ Assets/Drew Stuff/DrewBikePhysics.cs
@@ -3,43 +3,107 @@ using System.Collections;
 
 public class DrewBikePhysics : MonoBehaviour
 {
-
     public float speed = 10.0f;
     public float maxSpeed = 20.0f;
     public float minSpeed = 10.0f;
     public float steerSpeed = 10.0f;
     public float steerAngle = 30.0f;
+    public float deccelSpeed = 3.0f;
 
 	public Transform backTire;
 	public Transform frontTire;
 
-    private Vector3 rotDir;
+    private float accelFactor = 0.0f;
 
-	void FixedUpdate ()
+    private Vector3 rotDir;
+    private Vector3 moveDir;
+
+    private DrewBackTire drewBackTire;
+
+    void Awake()
+    {
+        drewBackTire = backTire.GetComponent<DrewBackTire>();
+    }
+
+    void Start()
+    {
+        rotDir.y = 90;
+    }
+
+	void Update ()
     {
         Movement();
 	}
-
+    
     void Movement()
     {
         //get inputs
-        float forwardInput = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float forwardInput = Input.GetAxis("Vertical");
         float steer = Input.GetAxis("Horizontal");
 
+        if (drewBackTire.isGrounded)
+        {
+            Physics.gravity = new Vector3(0, -9.81f, 0);
+            if (forwardInput != 0)
+            {
+                //accelerate
+                accelFactor = Mathf.MoveTowards(accelFactor, maxSpeed, forwardInput * speed * Time.deltaTime);
+            }
+            else
+            {
+                //deccelerate
+                accelFactor = Mathf.MoveTowards(accelFactor, 0, deccelSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            Physics.gravity = new Vector3(0, -20, 0);
+        }
+        //set max and min speeds
+        moveDir.z = Mathf.Clamp(moveDir.z, -accelFactor, accelFactor);
+        //apply speed values
+        moveDir = new Vector3(0, rigidbody.velocity.y, accelFactor);
+
+        //move
+        transform.Translate(moveDir * Time.deltaTime);
+
         //bank the bike
-        rotDir = new Vector3(transform.eulerAngles.x, rotDir.y, Mathf.LerpAngle(transform.eulerAngles.z, -steerAngle * steer, 0.3f));
+        rotDir = new Vector3(rotDir.x, rotDir.y, Mathf.LerpAngle(transform.eulerAngles.z, -steerAngle * steer, 0.3f));
         //turn the bike
         rotDir.y += steer * steerSpeed;
-        
+        rotDir.x = ClampAngle(transform.eulerAngles.x, 20);
+
         //apply the rotation values
         transform.eulerAngles = rotDir;
-        
-        Vector3 vel = rigidbody.velocity;
-        //set max and min velocities
-        vel.z = Mathf.Clamp(vel.z, -minSpeed, maxSpeed);
-        rigidbody.velocity = vel;
-        
-        //move forward
-        rigidbody.AddForce(transform.forward * forwardInput, ForceMode.Impulse);
+    }
+
+    float ClampAngle(float angle, float limit)
+    {
+        if (angle > 180)
+        {
+            float angleB = 360 - angle;
+
+            if (angleB > limit)
+            {
+                angle = 360 - limit;
+            }
+            else
+            {
+                return angle;
+            }
+        }
+        if (angle < 180)
+        {
+            if (angle > limit && angle < 360 - limit)
+            {
+                angle = limit;
+            }
+            else
+            {
+                return angle;
+            }
+        }
+
+        return angle;
     }
 }
