681eaab714632e0386fc26e7bf1333b9d0144f8b
patrick rasmussen <mcman407@gmail.com>
2013-08-20 11:03:02 -0400

    flythrough

---
+++ Assets/Scripts/flythroughScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flythroughScript : MonoBehaviour {
+	
+	public bool firstpass = true;
+	public GameObject thisCamera;
+	public GameObject theRoot;
+	public GameObject theBike;
+	// Use this for initialization
+	void Start () {
+	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(thisCamera.transform.position == theRoot.transform.position && ! firstpass )
+		{
+			thisCamera.gameObject.SetActive(false);
+			theBike.gameObject.SetActive(true);
+		}
+		else
+		{
+			firstpass = false;
+		}
+	}
+}
