0ec19c0052780a71a8449e95cae282e1f00752c8
dre38w <dre38w@gmail.com>
2013-08-19 21:57:41 -0400

    flipping

---
+++ Assets/Drew Stuff/CamLogic.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CamLogic : MonoBehaviour
+{
+    public GameObject[] allCrowds;
+    public GameObject crowd;
+
+    public Transform bike;
+
+    private DrewBikePhysics bikePhysics;
+
+	// Use this for initialization
+	void Start ()
+    {
+        bikePhysics = bike.GetComponent<DrewBikePhysics>();
+        allCrowds = GameObject.FindGameObjectsWithTag("Crowd");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TurboControl();
+	}
+
+    void TurboControl()
+    {
+        for (int i = 0; i < allCrowds.Length; i++)
+        {
+            //get direction to current crowd
+            Vector3 dirToCrowd = allCrowds[i].transform.position - transform.position;
+            //get forward direction
+            Vector3 forwardDir = transform.TransformDirection(Vector3.forward);
+
+            //the dot product
+            float dotProduct = Vector3.Dot(transform.forward, dirToCrowd.normalized);
+            
+            //looking in general direction
+            if (dotProduct > 0.9f)
+            {
+                //set current stand we are looking at
+                crowd = allCrowds[i];
+                //apply turbo
+                if (bikePhysics.turboBar < bikePhysics.maxTurboBar)
+                {
+                    bikePhysics.turboBar += bikePhysics.turboBoostSpeed * Time.deltaTime;
+                }
+            }
+        }
+    }
+}
--- Assets/Drew Stuff/DrewBikePhysics.cs
+++ Assets/Drew Stuff/DrewBikePhysics.cs
@@ -9,14 +9,29 @@ public class DrewBikePhysics : MonoBehaviour
     public float steerSpeed = 10.0f;
     public float steerAngle = 30.0f;
     public float deccelSpeed = 3.0f;
+    public float flipSpeed = 5.0f;
 
-	public Transform backTire;
+    public float flipAngle = 20.0f;
+    public float wheelieAngle = 100.0f;
+
+    [HideInInspector]
+    public float turboBar = 0.0f;
+    public float turboBoostSpeed = 12.0f;
+    public float maxTurboBar = 30.0f;
+    public float turboSpeed = 40.0f;
+    public float turboDepletionSpeed = 5.0f;
+
+    public Transform backTire;
 	public Transform frontTire;
+    public Transform bikeBody;
 
     private float accelFactor = 0.0f;
-
-    private Vector3 rotDir;
+    private float curMaxSpeed = 0.0f;
+    
+    public Vector3 rotDir;
     private Vector3 moveDir;
+
+    public Quaternion initRot;
 
     private DrewBackTire drewBackTire;
 
@@ -27,12 +42,17 @@ public class DrewBikePhysics : MonoBehaviour
 
     void Start()
     {
-        rotDir.y = 90;
+        initRot = bikeBody.rotation;
+
+        curMaxSpeed = maxSpeed;
+        //TEMPORARY
+        //rotDir.y = 90;
     }
 
 	void Update ()
     {
         Movement();
+        Turbo();
 	}
     
     void Movement()
@@ -40,14 +60,21 @@ public class DrewBikePhysics : MonoBehaviour
         //get inputs
         float forwardInput = Input.GetAxis("Vertical");
         float steer = Input.GetAxis("Horizontal");
+        float flipInput = Input.GetAxis("Flip");
 
         if (drewBackTire.isGrounded)
         {
+            //turn the bike
+            rotDir.y += steer * steerSpeed * Time.deltaTime;
+            //do a wheelie
+            //bikeBody.localRotation = Quaternion.RotateTowards(bikeBody.localRotation, Quaternion.Euler(wheelieAngle, 0, 0), flipSpeed * Time.deltaTime);
+            //reset gravity
             Physics.gravity = new Vector3(0, -9.81f, 0);
+
             if (forwardInput != 0)
             {
                 //accelerate
-                accelFactor = Mathf.MoveTowards(accelFactor, maxSpeed, forwardInput * speed * Time.deltaTime);
+                accelFactor = Mathf.MoveTowards(accelFactor, curMaxSpeed, forwardInput * speed * Time.deltaTime);
             }
             else
             {
@@ -57,10 +84,20 @@ public class DrewBikePhysics : MonoBehaviour
         }
         else
         {
-            Physics.gravity = new Vector3(0, -20, 0);
+            //rotate bike for a flip
+            bikeBody.Rotate(Vector3.right * flipSpeed * flipInput * Time.deltaTime);
+            //make gravity stronger
+            Physics.gravity = new Vector3(0, -14, 0);
         }
+
+        if (flipInput == 0)
+        {
+            //reset bike's rotation when not flipping
+            bikeBody.localRotation = Quaternion.RotateTowards(bikeBody.localRotation, initRot, flipSpeed * Time.deltaTime);
+        }
+        
         //set max and min speeds
-        moveDir.z = Mathf.Clamp(moveDir.z, -accelFactor, accelFactor);
+        moveDir.z = Mathf.Clamp(moveDir.z, -minSpeed, accelFactor);
         //apply speed values
         moveDir = new Vector3(0, rigidbody.velocity.y, accelFactor);
 
@@ -69,14 +106,31 @@ public class DrewBikePhysics : MonoBehaviour
 
         //bank the bike
         rotDir = new Vector3(rotDir.x, rotDir.y, Mathf.LerpAngle(transform.eulerAngles.z, -steerAngle * steer, 0.3f));
-        //turn the bike
-        rotDir.y += steer * steerSpeed;
-        rotDir.x = ClampAngle(transform.eulerAngles.x, 20);
+        //limit the x angle
+        rotDir.x = ClampAngle(transform.eulerAngles.x, flipAngle);
 
         //apply the rotation values
         transform.eulerAngles = rotDir;
     }
 
+    void Turbo()
+    {
+        //activate turbo
+        if (Input.GetButton("Jump") && turboBar > 0)
+        {
+            //deplete turbo
+            turboBar -= turboDepletionSpeed * Time.deltaTime;
+            //set turbo speed
+            curMaxSpeed = turboSpeed;
+        }
+        else
+        {
+            //reset speed
+            curMaxSpeed = maxSpeed;
+        }
+    }
+
+    //limit the angle
     float ClampAngle(float angle, float limit)
     {
         if (angle > 180)
