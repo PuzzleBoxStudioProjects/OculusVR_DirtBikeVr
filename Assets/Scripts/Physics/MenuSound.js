8f87b297b65ae4d4642fbe95c52ab30853bc971f
patrick rasmussen <mcman407@gmail.com>
2013-08-19 21:13:41 -0500

    files

---
+++ Assets/Scripts/Physics/MenuSound.js
@@ -0,0 +1,14 @@
+﻿private static var instance:MenuSound;
+public static function GetInstance() : MenuSound {
+return instance;
+}
+ 
+function Awake() {
+    if (instance != null && instance != this) {
+        Destroy(this.gameObject);
+        return;
+    } else {
+        instance = this;
+    }
+    DontDestroyOnLoad(this.gameObject);
+}
