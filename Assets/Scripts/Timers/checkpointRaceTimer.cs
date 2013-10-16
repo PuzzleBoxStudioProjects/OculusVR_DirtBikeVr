39adde4a9aad5a0ffe5f4d3aa075694ed03c88e7
Matthew Wilber <m.wilber98@gmail.com>
2013-10-15 21:23:02 -0400

    Added the timers
    Here is the main timer and the checkpoint timers when we get there

---
+++ Assets/Scripts/Timers/checkpointRaceTimer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkpointTimer : MonoBehaviour
+{
+    private lapTimer timer;
+
+    //checkpoint Timer Variables
+    float currentTime = 0.0f;
+    float lastTime = 0.0f;
+    float diffrenceTime = 0.0f;
+    float currentLapTime = 0.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = lapTimer.GetComponent<lapTimer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update the lap timers time with this one
+        currentLapTime = timer.currentLapTime;
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            //replace the lap before with the last time
+            currentTime = lastTime;
+            //save current time over last lap
+            currentTime = currentLapTime;
+            //calculate the diffrence
+            diffrenceTime = currentTime - lastTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Timers/mainRaceTimer.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lapTimer : MonoBehaviour
+{
+    //Lap Timer Variables
+    float fastestTime = 0.0f;
+    float totalTime = 0.0f;
+    static float currentLapTime = 0.0f;
+    float lastLapTime = 0.0f;
+
+    //Lap variables
+    int currentLap = 0;
+    int totalLap = 0;
+
+    //start variable
+    bool raceGoing = false;
+    bool raceStart = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        //read total laps
+        //read fastest lap
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if race started
+        if (raceStart == true)
+        {
+            currentLapTime = 0.0f;
+            raceGoing = true;
+        }
+        //if race is going time it
+        if (raceGoing == true)
+        {
+            currentLapTime += time.deltaTime;
+        }
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        //if the player collides with the finish line trigger
+        if (other.gameObject.tag == "player")
+        {
+                if (currentLap == totalLap && raceGoing == true)
+                {
+                    totalTime += currentLapTime;
+                    if (totalTime > fastestTime)
+                    {
+                        //replace the fastest time with current  
+                   
+                        //stop the race
+                        raceGoing = false;
+                    }
+                    else
+                    {
+                        //stop the race
+                        raceGoing = false;
+                    }
+                }
+                if (currentLap != totalLap && raceGoing == true)
+                {
+                    //add the time to total and save the lap time
+                    totalTime += currentLapTime;
+                    lastLapTime = currentLapTime;
+                    //reset the lap time
+                    currentLapTime = 0.0f;
+					currentLap += 1;
+                }
+        }
+    }
+}
